461860db6c3695710cd70cad1306e0d6c9ebaf3b
prabath sanjeewa <prabathslakmal@gmail.com>
2023-03-23 19:26:51 +0530

    kotee first commit

---
+++ src/main/java/lk/ijse/dep10/git/AppInitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep10.git;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Student
+(
+    id      VARCHAR(20) PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
+    address VARCHAR(400) NOT NULL
+);
